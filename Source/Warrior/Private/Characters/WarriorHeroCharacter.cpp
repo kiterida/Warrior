b969eb318257231e211d86fccf610d4e9ac9d91a
kiterida <jaicuyler@gmail.com>
2025-06-01 12:05:43 +1000

    Created Debug Helper

--- Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
+++ Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
@@ -2,4 +2,11 @@
 
 
 #include "Characters/WarriorHeroCharacter.h"
+#include "WarriorDebugHelper.h"
 
+void AWarriorHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Debug::Print(TEXT("Working"));
+}
