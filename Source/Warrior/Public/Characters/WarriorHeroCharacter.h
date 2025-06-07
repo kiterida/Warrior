b5475f27ab99535aa52e211fbfd2bc067e0fd818
kiterida <jaicuyler@gmail.com>
2025-06-07 16:42:14 +1000

    Added player move and look functions - custom input component done

--- Source/Warrior/Public/Characters/WarriorHeroCharacter.h
+++ Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -8,6 +8,8 @@
 
 class USpringArmComponent;
 class UCameraComponent;
+class UDataAsset_InputConfig;
+struct FInputActionValue;
 
 /**
  * 
@@ -20,6 +22,7 @@ class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 public:
 	AWarriorHeroCharacter();
 protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	virtual void BeginPlay() override;
 
 private:
@@ -30,6 +33,15 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FollowCamera;
 #pragma endregion
+	
 
+#pragma region Inputs
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
+	UDataAsset_InputConfig* InputConfigDataAsset;
+
+	void Input_Move(const FInputActionValue& InputActionValue);
+	void Input_Look(const FInputActionValue& InputActionValue);
+#pragma endregion
 	
 };
